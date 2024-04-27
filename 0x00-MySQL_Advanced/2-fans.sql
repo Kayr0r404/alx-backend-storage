511a015bc5631806f254f56000d1a1090de54ca7
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-27 16:44:21 +0200

    rank

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- Query to rank country origins of bands by number of fans
-SELECT origin, SUM(nb_fans) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;

ad08b52d75bf2ac27a813ac9a0b795b3c79a53ee
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-04-30 18:51:54 +0200

    connection create

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,13 +3,22 @@
 
 from pymongo import MongoClient
 
-client = MongoClient()
-db = client.logs
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("{} logs".format(db.nginx.count_documents({})))
-print("methods:")
-for method in methods:
-    print(
-        "\t method {}: {}".format(method, db.nginx.count_documents({"method": method}))
-    )
-print("{} status check".format(db.nginx.count_documents({"path": "/status"})))
+
+def log_stats():
+    """log status of nginx"""
+    client = MongoClient()
+    db = client.logs
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(db.nginx.count_documents({})))
+    print("methods:")
+    for method in methods:
+        print(
+            "\t method {}: {}".format(
+                method, db.nginx.count_documents({"method": method})
+            )
+        )
+    print("{} status check".format(db.nginx.count_documents({"path": "/status"})))
+
+
+if __name__ == "__main__":
+    log_stats()

cfff728772ce5830fb5a6a68d3ffa045be0dc523
Kayr0r404 <mahlarekarabo.702@gmail.com>
2024-05-03 20:25:22 +0200

    aggregate

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -8,29 +8,31 @@ def log_stats():
     """log status of nginx"""
     client = MongoClient("mongodb://127.0.0.1:27017")
     db = client.logs
+    collection = db.nginx
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("{} logs".format(db.nginx.count_documents({})))
+    print("{} logs".format(collection.count_documents({})))
     print("Methods:")
     for method in methods:
         print(
             "\tmethod {}: {}".format(
-                method, db.nginx.count_documents({"method": method})
+                method, collection.count_documents({"method": method})
             )
         )
-    print("{} status check".format(db.nginx.count_documents({"path": "/status"})))
+    print("{} status check".format(collection.count_documents({"path": "/status"})))
 
     print("IPs:")
-    request_logs = client.logs.nginx.aggregate(
+
+    cursor = collection.aggregate(
         [
-            {"$group": {"_id": "$ip", "totalRequests": {"$sum": 1}}},
-            {"$sort": {"totalRequests": -1}},
+            {"$group": {"_id": "$ip", "same ip": {"$sum": 1}}},
+            {"$sort": {"same ip": -1}},
             {"$limit": 10},
         ]
     )
-    for request_log in request_logs:
-        ip = request_log["_id"]
-        ip_requests_count = request_log["totalRequests"]
-        print("\t{}: {}".format(ip, ip_requests_count))
+    print(type(collection))
+
+    for i in cursor:
+        print((i))
 
 
 if __name__ == "__main__":
